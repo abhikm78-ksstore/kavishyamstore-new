4f8cf9a1e32c62739ebadb1fedc3d003cd6d90a7
abhikm78-ksstore <abhikm78@gmail.com>
2026-08-04 23:57:13 +0530

    Implement product search functionality
    Add a function to search and filter product cards based on user input.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-// KaviShyam Store JavaScript
+function searchProducts() {
+    let input = document.getElementById("search").value.toLowerCase();
+    let cards = document.getElementsByClassName("card");
+
+    for (let i = 0; i < cards.length; i++) {
+        let title = cards[i].getElementsByTagName("h3")[0].innerText.toLowerCase();
+
+        if (title.indexOf(input) > -1) {
+            cards[i].style.display = "";
+        } else {
+            cards[i].style.display = "none";
+        }
+    }
+}
